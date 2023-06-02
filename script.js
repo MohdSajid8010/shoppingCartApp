57eb7d0492f2fd61bc15df4c01a545da989e22aa
sajid <mohdsajid8010@gmail.com>
2023-06-03 00:50:52 +0530

    second commit

--- script.js
+++ script.js
@@ -2,57 +2,56 @@
 
 // myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
 
-document.getElementById("loginBtn").addEventListener("click",()=>{
+document.getElementById("loginBtn").addEventListener("click", () => {
     location.href = "./login/login.html";
 })
 
 
 
 
-document.getElementById("sighupBtn").addEventListener("click",()=>{
+document.getElementById("sighupBtn").addEventListener("click", () => {
 
-location.href="./signUp/sign-up.html"
+    location.href = "./signUp/sign-up.html"
 })
 
-let curr_user=JSON.parse(localStorage.getItem("curr_user"))||[];
-document.getElementById("myCart").addEventListener("click",()=>{
-    if(!curr_user ||curr_user.length==0)
-    {
+let curr_user = JSON.parse(localStorage.getItem("curr_user")) || [];
+document.getElementById("myCart").addEventListener("click", () => {
+    if (!curr_user || curr_user.length == 0) {
         alert("You are not login!");
 
-    }else{
+    } else {
         alert(`Already Login as ${curr_user.email}`);
 
-        location.href="./cart/mycart.html";
+        location.href = "./cart/mycart.html";
     }
 })
 
 
-document.getElementById("profile").addEventListener("click",()=>{
-    if(!curr_user ||curr_user.length==0)
-    {
+document.getElementById("profile").addEventListener("click", () => {
+    if (!curr_user || curr_user.length == 0) {
         alert("You are not login!");
 
-    }else{
+    } else {
         alert(`Already Login as ${curr_user.email}`);
 
-        location.href="../f3-project/profile/profile.html";
+        location.href = "./profile/profile.html";
     }
 })
 
 
-  if(localStorage.getItem("currentUser")){
-      window.location.href = "../shop/index.html";    
-  }
+if (localStorage.getItem("curr_user")) {
+    alert("You are Logged in!")
+    window.location.href = "./shop/shop.html";
+}
 
 
-  document.querySelector("#my-icon").addEventListener("click",()=>{
+document.querySelector("#my-icon").addEventListener("click", () => {
     if (document.querySelector("#my-icon").innerText == "menu") {
         document.querySelector("#my-icon").innerText = "close"
-      } else {
+    } else {
         document.querySelector("#my-icon").innerText = "menu"
-    
-      }
+
+    }
     document.querySelector(".nav-left").classList.toggle("nav-left2");
-   
-  })
+
+})
